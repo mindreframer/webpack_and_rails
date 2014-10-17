16d4bfab6453f52ccb7c88a40a36023156d794e6
Roman Heinrich <roman.heinrich@gmail.com>
2014-10-17 15:38:28 +0200

    noParse to suppress warnings

--- fe/webpack.config.js
+++ fe/webpack.config.js
@@ -67,7 +67,12 @@ module.exports = {
                 test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
                 loader: "file-loader?name=[path][name]-[hash].[ext]&size=6"
             }
-        ]
+        ],
+        noParse: [
+            /jquery/,
+            /react/,
+        ],
+
     },
     resolve: {
         alias: {
